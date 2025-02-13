4ef18229113ca9824098f3dc046c29690a74473b
VithanageVMS <vithanagevms@gmail.com>
2025-02-13 13:06:35 +1300

    Week6 tasks - Data cleaning

--- Week6-Data-Cleaning/Data-cleaning.py
+++ Week6-Data-Cleaning/Data-cleaning.py
@@ -2,19 +2,35 @@
 ################ Data cleaning the Iris dataset #################
 from sklearn import datasets
 import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
 
 # load iris dataset
 iris = datasets.load_iris()
 # Since this is a bunch, create a dataframe
+
 iris_df=pd.DataFrame(iris.data)
 iris_df['class']=iris.target
 
 iris_df.columns=['sepal_len', 'sepal_wid', 'petal_len', 'petal_wid', 'class']
 #### ===> TASK 1: here - add two more lines of the code to find the number and mean of missing data
+sum_missing = iris_df.isnull().values.sum()
+mean_missing = iris_df.isnull().values.mean()
+print(f"The total missig records are {sum_missing} and mean is {mean_missing}")
 cleaned_data = iris_df.dropna(how="all", inplace=True) # remove any empty lines
-
 
 iris_X=iris_df.iloc[:5,[0,1,2,3]]
 print(iris_X)
 
+print(iris_X.duplicated())
+
 ### TASK2: Here - Write a short readme to explain above code and how we can calculate the corrolation amoung featuers with description
+
+correlation_matrix = iris_df.iloc[:, :4].corr()  # Compute correlation for feature columns
+print(correlation_matrix)
+
+plt.figure(figsize=(8, 6))
+sns.heatmap(correlation_matrix, annot=True, cmap='coolwarm', fmt='.2f')
+plt.title("Correlation Heatmap of Iris Dataset Features")
+plt.show()
---
+++ Week6-Data-Cleaning/Readme.md
@@ -0,0 +1,3 @@
+Data-clenaing.py module trying to read the data from iris dataset into pandas dataframe.
+main target is to remove duplicate and analyse the features. 
+
